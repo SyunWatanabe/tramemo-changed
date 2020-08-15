53e45dff9c7a6557ddd11899b4e32063d0ea10da
shun <3939s.w@gmail.com>
2020-08-15 23:46:33 +0900

    add base routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,39 @@
-import * as React from 'react'
-import Link from 'next/link'
+import * as React from 'react';
+import Link from 'next/link';
+import * as ROUTES from '../constants/routes';
 
 const Header = ({ pathname }) => (
   <header>
-    <Link href="/">
-      <a className={pathname === '/' ? 'is-active' : ''}>Home</a>
-    </Link>{' '}
-    <Link href="/about">
-      <a className={pathname === '/about' ? 'is-active' : ''}>About</a>
+    <Link href={ROUTES.LANDING}>
+      <a className={pathname === ROUTES.LANDING ? 'is-active' : ''}>Home</a>
     </Link>
+    <Link href={ROUTES.ABOUT}>
+      <a className={pathname === ROUTES.ABOUT ? 'is-active' : ''}>About</a>
+    </Link>
+    <Link href={ROUTES.SIGN_IN}>
+      <a className={pathname === ROUTES.SIGN_IN ? 'is-active' : ''}>Sign In</a>
+    </Link>
+    <Link href={ROUTES.SIGN_UP}>
+      <a className={pathname === ROUTES.SIGN_UP ? 'is-active' : ''}>Sign UP</a>
+    </Link>
+    <Link href={ROUTES.LANDING}>
+      <a className={pathname === ROUTES.LANDING ? 'is-active' : ''}>Landing</a>
+    </Link>
+    <Link href={ROUTES.MYPAGE}>
+      <a className={pathname === ROUTES.MYPAGE ? 'is-active' : ''}>Mypage</a>
+    </Link>
+    <Link href={ROUTES.ACCOUNT}>
+      <a className={pathname === ROUTES.ACCOUNT ? 'is-active' : ''}>Account</a>
+    </Link>
+    <Link href={ROUTES.ADMIN}>
+      <a className={pathname === ROUTES.ADMIN ? 'is-active' : ''}>Admin</a>
+    </Link>
+    <style jsx>{`
+      a {
+        margin: 10px;
+      }
+    `}</style>
   </header>
-)
+);
 
-export default Header
+export default Header;
---
+++ src/constants/routes.jsx
@@ -0,0 +1,8 @@
+export const LANDING = '/';
+export const ABOUT = '/about';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const MYPAGE = '/mypage';
+export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
+export const PASSWORD_FORGET = '/pw-forget';
